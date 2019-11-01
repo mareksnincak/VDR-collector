75a1771703fbe66201e72ec3c0af6b4cceb86a19
majkojordan <m.snincak@gmail.com>
2019-11-01 15:29:57 +0100

    handle websocket failure && parameters

--- collector.js
+++ collector.js
@@ -6,15 +6,18 @@ const args = require('minimist')(process.argv.slice(2));
 const urllib = require('urllib');
 
 if (!args._ || !args.a || !args.p) {
-  console.log('Usage: node collector.js ADDRESS -a AUTH -p WS PORT');
+  console.log('Usage: node collector.js ADDRESS -a AUTH -p WS PORT [-o OUTPUT FOLDER] [-r NUMBER OF RETRIES]');
   return;
 }
 
 const ADDR = args._[0];
 const PORT = args.p;
 const AUTH = args.a;
+const MAX_RETRIES = Number(args.r) || 3;
+const OUTPUT_FOLDER = (args.o && args.o.replace(/(\/|\\)$/, '')) || 'data';
 
-const setRecordTypeDevelopment = async (value = 0, retry = 3) => {
+
+const setDevelopmentRecordStream = async (value = 0, allowedRetries = MAX_RETRIES) => {
   try {
     const result = await urllib.request(
       `http://${ADDR}/api/param/set`,
@@ -38,11 +41,11 @@ const setRecordTypeDevelopment = async (value = 0, retry = 3) => {
     const data = JSON.parse(result.data);
     if (data.result) throw data;
     
-    console.debug('development record streaming set');
+    console.debug('done');
   } catch (err) {
-    if (retry) {
+    if (allowedRetries) {
       console.debug('retrying...');
-      await setRecordTypeDevelopment(value, retry - 1);
+      await setDevelopmentRecordStream(value, allowedRetries - 1);
       return;
     }
 
@@ -50,52 +53,60 @@ const setRecordTypeDevelopment = async (value = 0, retry = 3) => {
   }
 };
 
+const collectRecords = (allowedRetries = MAX_RETRIES) => {
+  const ws = new WebSocket(`ws://${ADDR}:${PORT}`);
+    
+  console.log('waiting for device');
+
+  ws.on('open', () => {
+    console.log('connected');
+  });
+
+  ws.on('message', data => {
+    fs.writeFile(`${OUTPUT_FOLDER}/vdr-${Date.now()}.json`, data, err => {
+      if (err) throw err;
+      console.log('record saved');
+    });
+  });
+
+  ws.on('error', err => {
+    if (allowedRetries) {
+      console.debug('retrying...');
+      collectRecords(allowedRetries - 1);
+      return;
+    }
+
+    console.error(err);
+    return;
+  });
+};
+
 const init = async () => {
   try {
     console.debug('enabling development record streaming');
-    await setRecordTypeDevelopment(1);
+    await setDevelopmentRecordStream(1);
   } catch (err) {
     console.error('unable to set development record streaming\n', err);
     process.exit();
   }
 
-  const dir = 'data';
-  if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
+  if (!fs.existsSync(OUTPUT_FOLDER)){
+      fs.mkdirSync(OUTPUT_FOLDER);
   }
   
-  // connect to device
-  
-  const ws = new WebSocket(`ws://${ADDR}:${PORT}`);
-  
-  console.log('waiting for device');
-  
-  ws.on('open', () => {
-    console.log('connected');
-  });
-  
-  ws.on('message', data => {
-    fs.writeFile(`${dir}/vdr-${Date.now()}.json`, data, err => {
-      if (err) throw err;
-      console.log('record saved');
-    });
-  });
-  
-  ws.on('error', err => {
-    console.log(err);
-  });
+  collectRecords();
 };
 
 init();
 
 process.on('SIGINT', async () => {
-  // disable development record
+  // disable development record on exit
   try {
     console.debug('disabling development record streaming');
-    await setRecordTypeDevelopment(0);
+    await setDevelopmentRecordStream(0);
   } catch (err) {
     console.error('unable to set development record streaming\n', err);
   }
-  console.debug('disabled dev record saving');
+
   process.exit();
 });
